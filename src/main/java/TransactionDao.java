d6b3c6c32f54b4e04a14da09be0aff5323f0729a
Mateusz Dec <mateusz.dec92@gmail.com>
2020-01-10 14:16:16 +0100

    Dodawanie, modyfikacja, usuwanie danych

--- src/main/java/TransactionDao.java
+++ src/main/java/TransactionDao.java
@@ -107,36 +107,6 @@ public class TransactionDao {
         return null;
     }
 
-
-
-//    public Transaction showRevenue(String type) {
-//        Connection connection = connect();
-//
-//        PreparedStatement preparedStatement = null;
-//        try {
-//            String sql = "SELECT * FROM transaction WHERE type = ?";
-//            preparedStatement = connection.prepareStatement(sql);
-//            preparedStatement.setString(1, type);
-//            ResultSet resultSet = preparedStatement.executeQuery();
-//            if (resultSet.next()) {
-//                long id = resultSet.getLong("id");
-//                String typeFromDb = resultSet.getString("type");
-//                String description = resultSet.getString("description");
-//                Double amount = resultSet.getDouble("amount");
-//                String date = resultSet.getString("date");
-//
-//                Transaction transaction = new Transaction(id, typeFromDb, description, amount, date);
-//                return transaction;
-//            }
-//        } catch (SQLException e) {
-//            System.out.println("Niepowodzenie podczas dodania do bazy: " + e.getMessage());
-//        }
-//        closeConnection(connection);
-//        return null;
-//    }
-
-
-
     private void closeConnection(Connection connection) {
         try {
             connection.close();
